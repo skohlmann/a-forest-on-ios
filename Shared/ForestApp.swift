dba7a333b81c4b1402fcb77ccd3a77f81195507d
Sascha Kohlmann <iskohlmann@gmx.net>
2021-04-08 13:19:42 +0200

    Initial Commit

---
+++ Shared/ForestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestApp.swift
+//  Shared
+//
+//  Created by Sascha Kohlmann on 08.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ForestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
